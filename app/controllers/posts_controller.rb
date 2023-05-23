6edce5919612223e6a0988a718c94a5723cca510
mononoken <ken.maeshima@pm.me>
2023-05-22 19:49:00 -0700

    Fix mistake of treating Post model as controller and moved methods to posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirects_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,25 +1,2 @@
 class Post < ApplicationRecord
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-
-    if @post.save
-      redirects_to @post
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def index
-    @posts = Post.all
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
 end
